646e64bf644e81b3202e83881460760798a5003c
Asnow <ganrn@vip.qq.com>
2023-05-01 13:30:38 +0800

    feat(back-end): 访问日志

--- packages/back-end/src/main.ts
+++ packages/back-end/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { AppModule } from "./modules/app.module";
-import { HttpExceptionFilter } from "./filters/exp.filters";
-const PORT = process.env.MODE === "dev" ? 3000 : 80;
+import { HttpExceptionFilter } from "./filters/exp.filter";
+import { LoggerInterceptor } from "./interceptors/logger.interceptor";
+import * as config from "./config";
+import { loggerMiddleware } from "./middlewares/logger.middleware";
+
+const PORT = config.MODE === "dev" ? 3000 : 80;
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
     app.listen(PORT, "0.0.0.0");
-    app.useGlobalFilters(new HttpExceptionFilter());
-    if (process.env.PROD) console.log("start:" + PORT);
+    if (config.MODE === "dev") {
+        app.use(loggerMiddleware); //首页访问日志
+        app.useGlobalFilters(new HttpExceptionFilter());
+    }
+    if (config.LOGS_DIR) app.useGlobalInterceptors(new LoggerInterceptor());
+    console.log("start:" + PORT);
 }
 bootstrap();
