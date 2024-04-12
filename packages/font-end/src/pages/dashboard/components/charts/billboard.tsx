51bff94f13205c3bda0d40a6c3771aeb3b1a3c81
Eavid <ganrn@vip.qq.com>
2024-04-13 00:11:15 +0800

    refactor: 引入 echarts-comp

--- packages/font-end/src/pages/dashboard/components/charts/billboard.tsx
+++ packages/font-end/src/pages/dashboard/components/charts/billboard.tsx
@@ -1,76 +1,76 @@
 import { Card } from "@/components/card";
-import { Echarts } from "@/components/echarts";
+import { ECharts, EChartsOption } from "echarts-comp";
 import { Empty } from "antd";
-import type { EChartsOption, SeriesOption } from "echarts";
+import type { SeriesOption } from "echarts";
 import React, { CSSProperties, useEffect, useMemo, useRef, useState } from "react";
 
 export function Top20Billboard(props: {
-    data?: Record<string, any[]>;
-    title?: string;
-    legendNameMap: Record<string, string>;
-    dimensions: string[];
-    loading?: boolean;
-    style?: CSSProperties;
+  data?: Record<string, any[]>;
+  title?: string;
+  legendNameMap: Record<string, string>;
+  dimensions: string[];
+  loading?: boolean;
+  style?: CSSProperties;
 }) {
-    const { data = {}, title, loading, dimensions, legendNameMap } = props;
-    const keyList = useMemo(() => Object.keys(data), [data]);
-    let [showBoard, setShowBoard] = useState<string | undefined>();
-    if (!showBoard) showBoard = keyList[0];
-    const currentBoard = data[showBoard];
+  const { data = {}, title, loading, dimensions, legendNameMap } = props;
+  const keyList = useMemo(() => Object.keys(data), [data]);
+  let [showBoard, setShowBoard] = useState<string | undefined>();
+  if (!showBoard) showBoard = keyList[0];
+  const currentBoard = data[showBoard];
 
-    const echartsRef = useRef<echarts.ECharts>();
+  const echartsRef = useRef<ECharts>(null);
 
-    let height = 19 * (currentBoard?.length ?? 0) + 80;
-    const cityBoardOption = useMemo(() => {
-        if (!currentBoard || currentBoard.length === 0) {
-            return {
-                title: { text: title },
-            };
-        }
-        let series: SeriesOption = { type: "bar", label: { show: true, position: "insideLeft" } };
-        let option: EChartsOption = {
-            title: {
-                text: title,
-            },
+  let height = 19 * (currentBoard?.length ?? 0) + 80;
+  const cityBoardOption = useMemo(() => {
+    if (!currentBoard || currentBoard.length === 0) {
+      return {
+        title: { text: title },
+      };
+    }
+    let series: SeriesOption = { type: "bar", label: { show: true, position: "insideLeft" } };
+    let option: EChartsOption = {
+      title: {
+        text: title,
+      },
 
-            legend: {
-                selectedMode: "single",
-                formatter(name) {
-                    return legendNameMap[name];
-                },
-                top: 20,
-                right: 10,
-            },
-            grid: {
-                left: "3%",
-                right: "4%",
-                bottom: "3%",
-                containLabel: true,
-            },
-            dataset: {
-                dimensions,
-                source: currentBoard,
-            },
-            xAxis: {
-                type: "value",
-            },
-            yAxis: {
-                type: "category",
-            },
-            series: [series, series],
-        };
-        echartsRef.current?.resize({ height });
-        return option;
-    }, [currentBoard]);
+      legend: {
+        selectedMode: "single",
+        formatter(name) {
+          return legendNameMap[name];
+        },
+        top: 20,
+        right: 10,
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true,
+      },
+      dataset: {
+        dimensions,
+        source: currentBoard,
+      },
+      xAxis: {
+        type: "value",
+      },
+      yAxis: {
+        type: "category",
+      },
+      series: [series, series],
+    };
+    echartsRef.current?.resize({ height });
+    return option;
+  }, [currentBoard]);
 
-    useEffect(() => {
-        const charts = echartsRef.current;
-        charts?.on("legendselectchanged", (e: any) => setShowBoard(e.name));
-    }, [echartsRef.current]);
-    return (
-        <Card style={props.style}>
-            <Echarts ref={echartsRef} style={{ height }} option={cityBoardOption} noMerged state={{ loading }} />
-            {!currentBoard?.length ? <Empty /> : undefined}
-        </Card>
-    );
+  useEffect(() => {
+    const charts = echartsRef.current;
+    charts?.on("legendselectchanged", (e: any) => setShowBoard(e.name));
+  }, [echartsRef.current]);
+  return (
+    <Card style={props.style}>
+      <ECharts ref={echartsRef} style={{ height }} option={cityBoardOption} loading={loading} />
+      {!currentBoard?.length ? <Empty /> : undefined}
+    </Card>
+  );
 }
--- packages/font-end/src/pages/dashboard/components/charts/line-chart.tsx
+++ packages/font-end/src/pages/dashboard/components/charts/line-chart.tsx
@@ -1,73 +1,66 @@
 import { Card } from "@/components/card";
-import { Echarts } from "@/components/echarts";
+import { ECharts, EChartsOption } from "echarts-comp";
 import { Empty } from "antd";
-import { EChartsOption } from "echarts";
 import React, { CSSProperties, useEffect, useMemo, useRef } from "react";
 
 export function LineChart(props: {
-    data?: Record<string, any>[];
-    style?: CSSProperties;
-    loading?: boolean;
-    dimensions: string[];
-    nameMap?: Record<string, string>;
-    title: string;
+  data?: Record<string, any>[];
+  style?: CSSProperties;
+  loading?: boolean;
+  dimensions: string[];
+  nameMap?: Record<string, string>;
+  title: string;
 }) {
-    const { data, dimensions, nameMap = {}, title } = props;
-    const option = useMemo(() => {
-        if (!data?.length)
-            return {
-                title: {
-                    left: "center",
-                    text: title,
-                },
-            };
-        const option: EChartsOption = {
-            title: {
-                left: "center",
-                text: title,
-            },
-            tooltip: {
-                trigger: "axis",
-            },
-            xAxis: {
-                type: "category",
-            },
-            yAxis: { type: "value" },
+  const { data, dimensions, nameMap = {}, title } = props;
+  const option = useMemo(() => {
+    if (!data?.length)
+      return {
+        title: {
+          left: "center",
+          text: title,
+        },
+      };
+    const option: EChartsOption = {
+      title: {
+        left: "center",
+        text: title,
+      },
+      tooltip: {
+        trigger: "axis",
+      },
+      xAxis: {
+        type: "category",
+      },
+      yAxis: { type: "value" },
 
-            grid: {
-                left: "3%",
-                right: "4%",
-                bottom: "3%",
-                containLabel: true,
-            },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true,
+      },
 
-            dataset: {
-                dimensions,
-                source: data,
-            },
-            series: {
-                type: "line",
-                label: { show: true, position: "top" },
-            },
-        };
-        return option;
-    }, [data, dimensions]);
-    const ref = useRef<echarts.ECharts>();
-    const isEmpty = !data?.length;
-    useEffect(() => {
-        ref.current?.resize();
-    }, [isEmpty]);
+      dataset: {
+        dimensions,
+        source: data,
+      },
+      series: {
+        type: "line",
+        label: { show: true, position: "top" },
+      },
+    };
+    return option;
+  }, [data, dimensions]);
+  const ref = useRef<ECharts>(null);
+  const isEmpty = !data?.length;
+  useEffect(() => {
+    ref.current?.resize();
+  }, [isEmpty]);
 
-    return (
-        <Card style={props.style}>
-            <Echarts
-                ref={ref}
-                option={option}
-                noMerged
-                style={{ height: isEmpty ? 100 : "100%" }}
-                state={{ loading: props.loading }}
-            />
-            {isEmpty && <Empty />}
-        </Card>
-    );
+  return (
+    <Card style={props.style}>
+      <ECharts ref={ref} option={option} style={{ height: isEmpty ? 100 : "100%" }} loading={props.loading} />
+      {isEmpty && <Empty />}
+    </Card>
+  );
 }
